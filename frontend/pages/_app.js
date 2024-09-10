0b48bd2cd94b7678b982c2aed516a012a8f770d5
Mit-Gandhi <141493846+Mit-Gandhi@users.noreply.github.com>
2024-09-10 19:37:44 +0530

    Add files via upload

---
+++ frontend/pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'; // Make sure this import statement is at the top of the file
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
