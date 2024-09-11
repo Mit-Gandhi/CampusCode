a0ce5f544c45d4f0e6137cceddfaa126e426c6ea
Mit-Gandhi <141493846+Mit-Gandhi@users.noreply.github.com>
2024-09-11 18:29:34 +0530

    Delete frontend/_app.js

--- frontend/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css'; // Make sure this import statement is at the top of the file
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
